b7db3421a4ea6c4a0cb2d4c52375681d1c5131fd
marcostiribeiro <marcos.ti.ribeiro@gmail.com>
2019-05-26 20:41:37 -0300

    inicial

---
+++ ProgramaLoja/ProgramaLoja/Conta.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaLoja
+{
+    public class Conta
+    {
+
+        List<Transacao> transacoes = new List<Transacao>();
+        public List<Transacao> Transacoes
+        {
+            get
+            {
+                return transacoes;
+            }
+
+            set
+            {
+                transacoes = value;
+            }
+        }
+
+        public void AddTransacao(string ind, decimal valor)
+        {
+            Transacao tr = new Transacao(ind, valor);
+            transacoes.Add(tr);
+        }
+        public void AddTransacao(Transacao trans)
+        {
+            transacoes.Add(trans);
+        }
+
+        public List<Transacao> getLista()
+        {
+           return  transacoes;
+
+        }
+
+
+
+
+        public decimal getSaldo()
+        {
+            decimal saldo = 0;
+            foreach (Transacao item in transacoes)
+            {
+                saldo = saldo + item.valor;
+
+            }
+            return saldo;
+        }
+
+
+
+        //private decimal saldo;
+        //public void credito(decimal valor)
+        //{
+        //    this.saldo = this.saldo + valor;
+        //}
+        //public void Debitar(decimal valor)
+        //{
+
+        //    this.saldo = this.saldo - valor;
+        //}
+
+        //public decimal getSaldo()
+        //{
+        //    return saldo;
+
+        //}
+
+
+
+
+
+    }
+}
---
+++ ProgramaLoja/ProgramaLoja/Transacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaLoja
+{
+    public class Transacao
+    {
+        public string identificacao { get; set; }
+        public decimal valor { get; set; }
+
+        public Transacao()
+        {
+
+        }
+
+        public Transacao(string identificacao, decimal valor)
+        {
+            this.identificacao = identificacao;
+            this.valor = valor;
+        }
+    }
+}
